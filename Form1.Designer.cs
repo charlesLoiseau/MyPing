de7e103fde22959252ace7de1dfd4bdbc1b75fcc
charles loiseau <charles.loiseau@epitech.eu>
2018-08-24 23:35:27 +0200

    all code

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MyPing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chartPing = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Save = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartPing
+            // 
+            this.chartPing.AccessibleName = "Ping";
+            this.chartPing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chartPing.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartPing.Legends.Add(legend1);
+            this.chartPing.Location = new System.Drawing.Point(0, 0);
+            this.chartPing.Name = "chartPing";
+            this.chartPing.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 6;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Square;
+            series1.Name = "Ping";
+            this.chartPing.Series.Add(series1);
+            this.chartPing.Size = new System.Drawing.Size(831, 553);
+            this.chartPing.SuppressExceptions = true;
+            this.chartPing.TabIndex = 0;
+            this.chartPing.Text = "chartPing";
+            title1.Name = "Title1";
+            title1.Text = "Ping";
+            this.chartPing.Titles.Add(title1);
+            this.chartPing.Click += new System.EventHandler(this.chartPing_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick_1);
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save.Location = new System.Drawing.Point(739, 516);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Reset.Location = new System.Drawing.Point(739, 487);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Stop.Location = new System.Drawing.Point(739, 458);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(826, 551);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.chartPing);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MyPing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartPing)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataVisualization.Charting.Chart chartPing;
+        public System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+
+namespace MyPing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int DoWork()
+        {
+            string host = "1.1.1.1";
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 1500;
+            PingReply reply = pingSender.Send(host, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                int t = (int)reply.RoundtripTime;
+                return t;
+            }
+            return 1500;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick_1(object sender, EventArgs e)
+        {
+            chartPing.Series["Ping"].Points.AddY(DoWork());
+            if (chartPing.Series["Ping"].Points.Count > 50)
+            {
+                chartPing.ChartAreas["ChartArea1"].AxisX.ScaleView.Position = chartPing.Series["Ping"].Points.Count - 50;
+                chartPing.ChartAreas["ChartArea1"].AxisX.ScaleView.Size = 50;
+            }
+        }
+
+        private void chartPing_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            chartPing.SaveImage("C:\\Users\\49667\\Pictures\\ping.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png);
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            chartPing.Series["Ping"].Points.Clear();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = !timer.Enabled;
+            Stop.Text = timer.Enabled ? "Stop" : "Start";
+        }
+    }
+}
